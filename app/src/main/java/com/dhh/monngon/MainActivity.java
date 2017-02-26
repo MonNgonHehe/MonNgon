4208a05d629fba0cfa53f5f30d5abc32c65cd737
24duong@gmail.com <Haiduong24>
2017-02-26 22:09:02 +0700

    ngày 1 ---- (26/2/2017)--- thiết kế sơ sơ giao diện và luồng dữ liệu và chia việc ngày sau (27/2/2017)

--- app/src/main/java/com/dhh/monngon/MainActivity.java
+++ app/src/main/java/com/dhh/monngon/MainActivity.java
@@ -10,7 +10,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+/**
+ * 26/2/2017
+ *
+ */
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
