fc6dd20b86aabc468b9544dcbabc3e77cc0e0c09
lehong10 <lehong100196@gmail.com>
2017-02-26 20:43:39 +0700

    hồng púh

--- app/src/main/java/com/dhh/monngon/MainActivity.java
+++ app/src/main/java/com/dhh/monngon/MainActivity.java
@@ -13,6 +13,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/**
+ * 26/2/2710 Hồng điên sao th
+ */
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
