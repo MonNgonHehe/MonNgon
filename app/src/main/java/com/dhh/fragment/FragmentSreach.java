d9eebbb1c1c52b60f781efd58ddce2582a378a06
lehong10 <lehong100196@gmail.com>
2017-02-28 21:50:15 +0700

    searchview

---
+++ app/src/main/java/com/dhh/fragment/FragmentSreach.java
@@ -0,0 +1,37 @@
+package com.dhh.fragment;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.dhh.activity.R;
+
+
+/**
+ * Created by Hong on 2/28/2017.
+ */
+
+public class FragmentSreach extends Fragment {
+    private ListView listView;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+//    @Nullable
+//    @Override
+
+//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//       // return super.onCreateView(inflater, container, savedInstanceState);
+//      //  View view =inflater.inflate(R.layout.fragment_mon_an,)
+//    }
+
+}
